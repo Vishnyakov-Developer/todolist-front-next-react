6df8ad1265b3e9896585c42919649cdaf3d5936d
Vishnyakov-Developer <officialvishnyakov@gmail.com>
2025-06-13 10:11:40 +0700

    fix

--- requests/config.js
+++ requests/config.js
@@ -1,6 +1,6 @@
 export const config = {
-  // url: 'https://back.vishnyakov.digital',
-  url: 'http://localhost:5500',
+  url: 'https://back.vishnyakov.digital',
+  // url: 'http://localhost:5500',
   getInitData() {
     if (typeof localStorage !== 'undefined') {
       return localStorage.getItem('__initdata')
